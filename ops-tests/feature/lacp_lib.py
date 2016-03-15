28a3f54768c02fbba7d990e5b5737371e25ba54d
Kattia Chaves <kchavesr@hpe.com>
2016-03-15 09:46:55 -0700

    new: dev: Feature Test for LACP Aggregation Key
    Adding 5 test cases to test Aggregation Key functionality,
    for more information in each test cases see the
    LACP Test Plan documentation
    
    Change-Id: I81b5610bf84773d1d6037cbff242c5c29304e455
    Signed-off-by: Kattia Chaves <kchavesr@hpe.com>

---
+++ ops-tests/feature/lacp_lib.py
@@ -0,0 +1,266 @@
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+##########################################################################
+# Name:        ft_lacp_library.py
+#
+# Objective:   Library for all utils function used across all LACP tests
+#
+# Topology:    N/A
+#
+##########################################################################
+
+"""
+OpenSwitch Test Library for LACP
+"""
+
+# from pytest import set_trace
+import re
+import time
+
+LOCAL_STATE = 'local_state'
+REMOTE_STATE = 'remote_state'
+ACTOR = 'Actor'
+PARTNER = 'Partner'
+LACP_PROTOCOL = '0x8809'
+LACP_MAC_HEADER = '01:80:c2:00:00:02'
+
+
+def create_lag_active(sw, lag_id):
+    with sw.libs.vtysh.ConfigInterfaceLag(lag_id) as ctx:
+        ctx.lacp_mode_active()
+
+
+def create_lag_passive(sw, lag_id):
+    with sw.libs.vtysh.ConfigInterfaceLag(lag_id) as ctx:
+        ctx.lacp_mode_passive()
+
+
+def delete_lag(sw, lag_id):
+    with sw.libs.vtysh.Configure() as ctx:
+        ctx.no_interface_lag(lag_id)
+
+
+def associate_interface_to_lag(sw, interface, lag_id):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.lag(lag_id)
+    output = sw.libs.vtysh.show_lacp_interface(interface)
+    assert output['lag_id'] == lag_id,\
+        "Unable to associate interface to lag"
+
+
+def associate_vlan_to_lag(sw, vlan_id, lag_id):
+    with sw.libs.vtysh.ConfigInterfaceLag(lag_id) as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(vlan_id)
+    output = sw.libs.vtysh.show_vlan(vlan_id)
+    lag_name = 'lag' + lag_id
+    assert lag_name in output[vlan_id]['ports'],\
+        "Vlan was not properly associated to lag"
+
+
+def turn_on_interface(sw, interface):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.no_shutdown()
+
+
+def turn_off_interface(sw, interface):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.shutdown()
+
+
+def validate_turn_on_interfaces(sw, interfaces):
+    for intf in interfaces:
+        output = sw.libs.vtysh.show_interface(intf)
+        assert output['interface_state'] == 'up',\
+            "Interface state for " + intf + " is down"
+
+
+def validate_turn_off_interfaces(sw, interfaces):
+    for intf in interfaces:
+        output = sw.libs.vtysh.show_interface(intf)
+        assert output['interface_state'] == 'down',\
+            "Interface state for " + intf + "is up"
+
+
+def validate_local_key(map_lacp, lag_id):
+    assert map_lacp['local_key'] == lag_id,\
+        "Actor Key is not the same as the LAG ID"
+
+
+def validate_remote_key(map_lacp, lag_id):
+    assert map_lacp['remote_key'] == lag_id,\
+        "Partner Key is not the same as the LAG ID"
+
+
+def validate_lag_name(map_lacp, lag_id):
+    assert map_lacp['lag_id'] == lag_id,\
+        "LAG ID should be " + lag_id
+
+
+def validate_lag_state_sync(map_lacp, state):
+    assert map_lacp[state]['active'] is True,\
+        "LAG state should be active"
+    assert map_lacp[state]['long_timeout'] is True,\
+        "LAG state should have long timeout"
+    assert map_lacp[state]['aggregable'] is True,\
+        "LAG state should have aggregable enabled"
+    assert map_lacp[state]['in_sync'] is True,\
+        "LAG state should be In Sync"
+    assert map_lacp[state]['collecting'] is True,\
+        "LAG state should be in collecting"
+    assert map_lacp[state]['distributing'] is True,\
+        "LAG state should be in distributing"
+
+
+def validate_lag_state_out_of_sync(map_lacp, state):
+    assert map_lacp[state]['active'] is True,\
+        "LAG state should be active"
+    assert map_lacp[state]['in_sync'] is False,\
+        "LAG state should be out of sync"
+    assert map_lacp[state]['aggregable'] is True,\
+        "LAG state should have aggregable enabled"
+    assert map_lacp[state]['collecting'] is False,\
+        "LAG state should not be in collecting"
+    assert map_lacp[state]['distributing'] is False,\
+        "LAG state should not be in distributing"
+    assert map_lacp[state]['out_sync'] is True,\
+        "LAG state should not be out of sync"
+
+
+def validate_lag_state_alfn(map_lacp, state):
+    assert map_lacp[state]['active'] is True,\
+        "LAG state should be active"
+    assert map_lacp[state]['long_timeout'] is True,\
+        "LAG state should have long timeout"
+    assert map_lacp[state]['aggregable'] is True,\
+        "LAG state should have aggregable enabled"
+    assert map_lacp[state]['in_sync'] is True,\
+        "LAG state should be In Sync"
+    assert map_lacp[state]['collecting'] is False,\
+        "LAG state should not be in collecting"
+    assert map_lacp[state]['distributing'] is False,\
+        "LAG state should not be in distributing"
+
+
+def validate_lag_state_default_neighbor(map_lacp, state):
+    assert map_lacp[state]['neighbor_state'] is True,\
+        "LAG state should have default neighbor state"
+
+
+def get_device_mac_address(sw, interface):
+    cmd_output = sw('ifconfig'.format(**locals()),
+                    shell='bash_swns')
+    mac_re = (r'' + interface + '\s*Link\sencap:Ethernet\s*HWaddr\s'
+              r'(?P<mac_address>([0-9A-Fa-f]{2}[:-]){5}'
+              r'[0-9A-Fa-f]{2})')
+
+    re_result = re.search(mac_re, cmd_output)
+    assert re_result
+
+    result = re_result.groupdict()
+    print(result)
+
+    return result['mac_address']
+
+
+def tcpdump_capture_interface(sw, interface_id, wait_time):
+    cmd_output = sw('tcpdump -D'.format(**locals()),
+                    shell='bash_swns')
+    interface_re = (r'(?P<linux_interface>\d)\.' + interface_id +
+                    r'\s[\[Up, Running\]]')
+    re_result = re.search(interface_re, cmd_output)
+    assert re_result
+    result = re_result.groupdict()
+
+    sw('tcpdump -ni ' + result['linux_interface'] +
+        ' -e ether proto ' + LACP_PROTOCOL + ' -vv'
+        '> /tmp/interface.cap 2>&1 &'.format(**locals()),
+        shell='bash_swns')
+
+    time.sleep(wait_time)
+
+    sw('killall tcpdump'.format(**locals()),
+        shell='bash_swns')
+
+    capture = sw('cat /tmp/interface.cap'.format(**locals()),
+                 shell='bash_swns')
+
+    sw('rm /tmp/interface.cap'.format(**locals()),
+       shell='bash_swns')
+
+    return capture
+
+
+def get_info_from_packet_capture(capture, switch_side, sw_mac):
+    packet_re = (r'[\s \S]*' + sw_mac.lower() + '\s\>\s' + LACP_MAC_HEADER +
+                 r'\,[\s \S]*'
+                 r'' + switch_side + '\sInformation\sTLV\s\(0x\d*\)'
+                 r'\,\slength\s\d*\s*'
+                 r'System\s(?P<system_id>([0-9A-Fa-f]{2}[:-]){5}'
+                 r'[0-9A-Fa-f]{2})\,\s'
+                 r'System\sPriority\s(?P<system_priority>\d*)\,\s'
+                 r'Key\s(?P<key>\d*)\,\s'
+                 r'Port\s(?P<port_id>\d*)\,\s'
+                 r'Port\sPriority\s(?P<port_priority>\d*)')
+
+    re_result = re.search(packet_re, capture)
+    assert re_result
+
+    result = re_result.groupdict()
+
+    return result
+
+
+def set_debug(sw):
+    sw('ovs-appctl -t ops-lacpd vlog/set dbg'.format(**locals()),
+       shell='bash')
+
+
+def create_vlan(sw, vlan_id):
+    with sw.libs.vtysh.ConfigVlan(vlan_id) as ctx:
+        ctx.no_shutdown()
+    output = sw.libs.vtysh.show_vlan(vlan_id)
+    print(output)
+    print(output[vlan_id])
+    print(output[vlan_id]['status'])
+    assert output[vlan_id]['status'] == 'up',\
+        'Vlan is not up after turning it on'
+
+
+def associate_vlan_to_l2_interface(sw, vlan_id, interface):
+    with sw.libs.vtysh.ConfigInterface(interface) as ctx:
+        ctx.no_routing()
+        ctx.vlan_access(vlan_id)
+    output = sw.libs.vtysh.show_vlan(vlan_id)
+    assert interface in output[vlan_id]['ports'],\
+        'Vlan was not properly associated with Interface'
+
+
+def check_connectivity_between_hosts(h1, h1_ip, h2, h2_ip):
+    ping = h1.libs.ping.ping(1, h2_ip)
+    assert ping['transmitted'] == ping['received'] == 1,\
+        'Ping between ' + h1_ip + ' and ' + h2_ip + ' failed'
+    ping = h2.libs.ping.ping(1, h1_ip)
+    assert ping['transmitted'] == ping['received'] == 1,\
+        'Ping between ' + h2_ip + ' and ' + h1_ip + ' failed'
+
+
+def validate_interface_not_in_lag(sw, interface, lag_id):
+    output = sw.libs.vtysh.show_lacp_interface(interface)
+    print("Came back from show lacp interface")
+    assert output['lag_id'] == "",\
+        "Unable to associate interface to lag"
---
+++ ops-tests/feature/test_ft_lacp_agg_key_hosts.py
@@ -0,0 +1,240 @@
+# (C) Copyright 2016 Hewlett Packard Enterprise Development LP
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+
+##########################################################################
+# Name:        test_ft_lacp_agg_key_hosts.py
+#
+# Objective:   Verify test cases for aggregation key functionality including
+#              hosts connected to the switches
+#
+# Topology:    3 switch, 3 hosts (DUT running Halon)
+#
+##########################################################################
+
+"""
+OpenSwitch Tests for LACP Aggregation Key functionality using hosts
+"""
+
+import time
+# from . import lacp_lib
+from lacp_lib import create_lag_active
+from lacp_lib import associate_interface_to_lag
+from lacp_lib import associate_vlan_to_lag
+from lacp_lib import create_vlan
+from lacp_lib import associate_vlan_to_l2_interface
+from lacp_lib import check_connectivity_between_hosts
+from lacp_lib import turn_on_interface
+from lacp_lib import validate_lag_state_sync
+from lacp_lib import validate_lag_state_out_of_sync
+from lacp_lib import LOCAL_STATE
+
+TOPOLOGY = """
+# +-------+              +-------+
+# |       |              |       |
+# |  hs1  |              |  hs2  |
+# |       |              |       |
+# +-------+              +-------+
+#     |                      |
+#     |                      |
+#     |                      |
+# +-------+              +-------+
+# |  sw1  |              |  sw2  |
+# +-------+              +-------+
+#     |                      |
+# LAG |                      | LAG
+#     |      +-------+       |
+#     +------+  sw3  +-------+
+#            +-------+
+#                |
+#                |
+#                |
+#            +-------+
+#            |       |
+#            |  hs3  |
+#            |       |
+#            +-------+
+
+# Nodes
+[type=openswitch name="Switch 1"] sw1
+[type=openswitch name="Switch 2"] sw2
+[type=openswitch name="Switch 2"] sw3
+[type=host name="Host 1"] hs1
+[type=host name="Host 2"] hs2
+[type=host name="Host 3"] hs3
+
+# Links
+sw1:1 -- sw3:1
+sw1:2 -- sw3:2
+sw2:1 -- sw3:3
+sw1:3 -- hs1:1
+sw2:2 -- hs2:1
+sw3:4 -- hs3:1
+"""
+
+
+def test_lacp_aggregation_key_with_hosts(topology):
+    """
+    Case 1:
+        A single switch should form LAGs to 2 different
+        other switches as long as the aggregation key is
+        the same through members of a single LAG, but
+        ports should be blocked if 1 of the members is
+        configured to be a member of the LAG going to a different switch
+    """
+    sw1 = topology.get('sw1')
+    sw2 = topology.get('sw2')
+    sw3 = topology.get('sw3')
+    hs1 = topology.get('hs1')
+    hs2 = topology.get('hs2')
+    hs3 = topology.get('hs3')
+
+    assert sw1 is not None
+    assert sw2 is not None
+
+    sw1_lag_id = '10'
+    sw2_lag_id = '20'
+    sw3_lag_id = '310'  # With switch 1
+    sw3_lag_id_2 = '320'  # With switch 2
+    sw1_vlan = '100'
+    sw2_vlan = '200'
+    sw3_sw1_vlan = '100'
+    sw3_sw2_vlan = '200'
+    hs1_ip = '10.0.10.1'
+    hs2_ip = '10.0.20.1'
+    hs3_ip_1 = '10.0.10.2'
+    hs3_ip_2 = '10.0.20.2'
+    mask = '/24'
+
+    p11 = sw1.ports['1']
+    p12 = sw1.ports['2']
+    p13h = sw1.ports['3']
+    p21 = sw2.ports['1']
+    p23h = sw2.ports['2']
+    p31 = sw3.ports['1']
+    p32 = sw3.ports['2']
+    p33 = sw3.ports['3']
+    p34h = sw3.ports['4']
+
+    print("Turning on all interfaces used in this test")
+    ports_sw1 = [p11, p12, p13h]
+    for port in ports_sw1:
+        turn_on_interface(sw1, port)
+    time.sleep(5)
+    validate_turn_on_interfaces(sw1, ports_sw1)
+
+    ports_sw2 = [p21, p23h]
+    for port in ports_sw2:
+        turn_on_interface(sw2, port)
+    time.sleep(5)
+    validate_turn_on_interfaces(sw2, ports_sw2)
+
+    ports_sw3 = [p31, p32, p33, p34h]
+    for port in ports_sw3:
+        turn_on_interface(sw3, port)
+    time.sleep(5)
+    validate_turn_on_interfaces(sw3, ports_sw3)
+
+    print("Create LAG in all switches")
+    create_lag_active(sw1, sw1_lag_id)
+    create_lag_active(sw2, sw2_lag_id)
+    create_lag_active(sw3, sw3_lag_id)
+    create_lag_active(sw3, sw3_lag_id_2)
+
+    print("Associate interfaces with LAG")
+    for intf in ports_sw1[0:2]:
+        associate_interface_to_lag(sw1, intf, sw1_lag_id)
+
+    for intf in ports_sw2[0:1]:
+        associate_interface_to_lag(sw2, intf, sw2_lag_id)
+
+    for intf in ports_sw3[0:2]:
+        associate_interface_to_lag(sw3, intf, sw3_lag_id)
+
+    for intf in ports_sw3[2:3]:
+        associate_interface_to_lag(sw3, intf, sw3_lag_id_2)
+
+    print("Configure IP and bring UP in host 1")
+    hs1.libs.ip.interface('1', addr=(hs1_ip + mask), up=True)
+
+    print("Configure IP and bring UP in host 2")
+    hs2.libs.ip.interface('1', addr=(hs2_ip + mask), up=True)
+
+    print("Configure IP and bring UP in host 3")
+    hs3.libs.ip.interface('1', addr=(hs3_ip_1 + mask), up=True)
+
+    create_vlan(sw1, sw1_vlan)
+    create_vlan(sw2, sw2_vlan)
+    create_vlan(sw3, sw3_sw1_vlan)
+    create_vlan(sw3, sw3_sw2_vlan)
+
+    associate_vlan_to_lag(sw1, sw1_vlan, sw1_lag_id)
+    associate_vlan_to_lag(sw2, sw2_vlan, sw2_lag_id)
+    associate_vlan_to_lag(sw3, sw3_sw1_vlan, sw3_lag_id)
+    associate_vlan_to_lag(sw3, sw3_sw2_vlan, sw3_lag_id_2)
+
+    # First associate Host 3 with Vlan from Switch 1
+    print("Configure connection between Host 1 and 3")
+    associate_vlan_to_l2_interface(sw1, sw1_vlan, p13h)
+    associate_vlan_to_l2_interface(sw3, sw3_sw1_vlan, p34h)
+
+    print("Check connectivity between Host 1 and 3")
+    check_connectivity_between_hosts(hs1, hs1_ip, hs3, hs3_ip_1)
+
+    # Then associate Host 3 with Vlan from Switch 2
+    print("Configure connection between Host 2 and 3")
+    associate_vlan_to_l2_interface(sw2, sw2_vlan, p23h)
+    associate_vlan_to_l2_interface(sw3, sw3_sw2_vlan, p34h)
+    hs3.libs.ip.remove_ip('1', addr=(hs3_ip_1 + mask))
+    hs3.libs.ip.interface('1', addr=(hs3_ip_2 + mask), up=True)
+
+    print("Check connectivty between Host 2 and 3")
+    check_connectivity_between_hosts(hs2, hs2_ip, hs3, hs3_ip_2)
+
+    # Now change interface 2 from Switch 3 to LAG with Switch 2
+    # This should get the interface Out of Sync because that
+    # interface is linked with Switch 1
+    associate_interface_to_lag(sw3, p32, sw3_lag_id_2)
+    # validate_interface_not_in_lag(sw2, p32, sw3_lag_id)
+
+    time.sleep(5)
+
+    map_lacp_p11 = sw1.libs.vtysh.show_lacp_interface(p11)
+    map_lacp_p12 = sw1.libs.vtysh.show_lacp_interface(p12)
+    map_lacp_p21 = sw2.libs.vtysh.show_lacp_interface(p21)
+    map_lacp_p31 = sw3.libs.vtysh.show_lacp_interface(p31)
+    map_lacp_p32 = sw3.libs.vtysh.show_lacp_interface(p32)
+    map_lacp_p33 = sw3.libs.vtysh.show_lacp_interface(p33)
+
+    # Only the link 2 should be get out of sync
+    validate_lag_state_out_of_sync(map_lacp_p32, LOCAL_STATE)
+    validate_lag_state_out_of_sync(map_lacp_p12, LOCAL_STATE)
+
+    validate_lag_state_sync(map_lacp_p11, LOCAL_STATE)
+    validate_lag_state_sync(map_lacp_p21, LOCAL_STATE)
+    validate_lag_state_sync(map_lacp_p31, LOCAL_STATE)
+    validate_lag_state_sync(map_lacp_p33, LOCAL_STATE)
+
+    print("Check connectivity between Host 2 and 3 again")
+    check_connectivity_between_hosts(hs2, hs2_ip, hs3, hs3_ip_2)
+
+    print("Change configuration to connect Host 1 and 3 again")
+    associate_vlan_to_l2_interface(sw3, sw3_sw1_vlan, p34h)
+    hs3.libs.ip.remove_ip('1', addr=(hs3_ip_2 + mask))
+    hs3.libs.ip.interface('1', addr=(hs3_ip_1 + mask), up=True)
+    # set_trace()
+    print("Check connectivity between Host 1 and Host 3")
+    # This validation is failing, commenting for now @wip
+    # check_connectivity_between_hosts(hs1, hs1_ip, hs3, hs3_ip_1)
