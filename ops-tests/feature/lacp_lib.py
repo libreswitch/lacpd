98087a5d85a5ff3f8a0a990569fe9a403a5f1b6b
Juan Gonzalez <jbgonzalez@hpe.com>
2016-05-05 15:23:04 -0700

    new: dev: Add new case test_ft_lag_static_maximum_members.py
    Case migrated from StaticLagDeleteMaxNumberOfMembers.py
    
    Added one function on lacp_lib.py
        verify_connectivity_between_hosts: Verify connectivity with retry wrapper
    
    Change-Id: Id66bf00ad07bd20444a06cf30aa2f2950d71fbae
    Signed-off-by: Juan Gonzalez <jbgonzalez@hpe.com>

--- ops-tests/feature/lacp_lib.py
+++ ops-tests/feature/lacp_lib.py
@@ -769,6 +769,17 @@ def verify_state_sync_lag(sw, port_list, state, lacp_mode):
     check_lag(sw, port_list, state, lacp_mode)
 
 
+def verify_connectivity_between_hosts(h1, h1_ip, h2, h2_ip, success=True):
+    @retry_wrapper(
+        'Ensure connectivity between hosts',
+        'LAG not yet ready',
+        5,
+        40)
+    def check_ping(h1, h1_ip, h2, h2_ip, success):
+        check_connectivity_between_hosts(h1, h1_ip, h2, h2_ip, success=success)
+    check_ping(h1, h1_ip, h2, h2_ip, success=success)
+
+
 def compare_lag_interface_basic_settings(
     lag_stats,
     lag_id,
